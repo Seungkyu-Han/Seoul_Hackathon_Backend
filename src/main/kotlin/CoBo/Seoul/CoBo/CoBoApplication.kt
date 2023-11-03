fbf3b084cec9c86678c5b36e3f24d725804c7998
trust1204@gmail.com <>
2023-11-04 00:31:33 +0900

    [feat] 스케줄러 작성

--- src/main/kotlin/CoBo/Seoul/CoBo/CoBoApplication.kt
+++ src/main/kotlin/CoBo/Seoul/CoBo/CoBoApplication.kt
@@ -2,8 +2,10 @@ package CoBo.Seoul.CoBo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class CoBoApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/CoBo/Seoul/CoBo/Scheduler/SendMail.kt
+++ src/main/kotlin/CoBo/Seoul/CoBo/Scheduler/SendMail.kt
@@ -1,18 +1,35 @@
 package CoBo.Seoul.CoBo.Scheduler
 
 import CoBo.Seoul.CoBo.Repository.UserRepository
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.lang.RuntimeException
+import javax.mail.MessagingException
 
 @Component
-class SendMail(val userRepository: UserRepository){
+class SendMail(
+    val userRepository: UserRepository,
+    val javaMailSender: JavaMailSender){
 
-    @Scheduled(cron = "0 0 0 * * *")
+    @Scheduled(cron = "0 0 * * * *")
     fun sendMail(){
+        println("메일 전송 시작")
         val userList = userRepository.findAll()
 
         for(user in userList){
-
+            val mimeMessage = javaMailSender.createMimeMessage()
+            return try{
+                val helper = MimeMessageHelper(mimeMessage, true, "UTF-8")
+                helper.setTo(user.email)
+                helper.setSubject("한강 자전거 보고서입니다.")
+                helper.setText("", true)
+                javaMailSender.send(mimeMessage)
+            }catch(e:MessagingException){
+                println("에러 발생")
+                throw RuntimeException(e)
+            }
         }
     }
 }
