4720c3f01505a7b9bd14e73af7978d9680f898f8
trust1204@gmail.com <>
2023-11-03 10:30:00 +0900

    start

---
+++ src/main/kotlin/CoBo/Seoul/CoBo/CoBoApplication.kt
@@ -0,0 +1,11 @@
+package CoBo.Seoul.CoBo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoBoApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoBoApplication>(*args)
+}
